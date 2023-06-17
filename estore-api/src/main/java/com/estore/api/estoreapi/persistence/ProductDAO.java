efb50b7fbb090bb7a3e2fc52bfaab29d06387065
jwh1000 <jwh1000@rit.edu>
2023-06-17 19:07:10 -0400

    changed ProductDAO to interface and added create method

--- estore-api/src/main/java/com/estore/api/estoreapi/persistence/ProductDAO.java
+++ estore-api/src/main/java/com/estore/api/estoreapi/persistence/ProductDAO.java
@@ -1,5 +1,17 @@
 package com.estore.api.estoreapi.persistence;
 
-public class ProductDAO {
-    
+import java.io.IOException;
+import com.estore.api.estoreapi.model.Product;
+
+public interface ProductDAO {
+    /**
+     * Creates and saves a {@linkplain Product product}
+     * 
+     * @param product {@linkplain Product product} object to be created
+     * 
+     * @return new {@link Product product} if successful, fail otherwise
+     * 
+     * @throws IOException if there is issue with underlying storage
+     */
+    Product creaProduct(Product product) throws IOException;
 }
