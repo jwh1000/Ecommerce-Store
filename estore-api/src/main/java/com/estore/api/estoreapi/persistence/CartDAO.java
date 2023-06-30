134f0115f7b9e6297fef63719cc6a512c3047724
jwh1000 <jwh1000@rit.edu>
2023-06-30 16:37:06 -0400

    added cart persistence files

---
+++ estore-api/src/main/java/com/estore/api/estoreapi/persistence/CartDAO.java
@@ -0,0 +1,20 @@
+package com.estore.api.estoreapi.persistence;
+
+import java.io.IOException;
+
+import com.estore.api.estoreapi.model.Product;
+
+public interface CartDAO {
+    //TODO documentation
+    
+
+    Product[] getCartContents() throws IOException;
+
+    Product[] searchCart(String containsText) throws IOException;
+
+    Product getCartProduct(int id) throws IOException;
+
+    Product addToCart(Product product) throws IOException;
+
+    boolean removeFromCart (int id) throws IOException;
+}
