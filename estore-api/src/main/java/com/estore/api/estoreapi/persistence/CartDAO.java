2d42cdaf9c879041efcc01f9f218906862bc1b24
syth <cjdeni205@gmail.com>
2023-07-03 19:39:27 -0400

    Added documentation for removeFromCart

--- estore-api/src/main/java/com/estore/api/estoreapi/persistence/CartDAO.java
+++ estore-api/src/main/java/com/estore/api/estoreapi/persistence/CartDAO.java
@@ -15,5 +15,15 @@ public interface CartDAO {
 
     Product addToCart(Product product, String usernam) throws IOException;
 
+    /**
+     * Deletes a product with a given id.
+     * 
+     * @param id The id of the product to delete.
+     * @param usernam The username of the owner of a shopping cart.
+     * @return Boolean of whether or not the product deletion was successful.
+     * @throws IOException if underlying storage cannot be accessed
+     * 
+     * @author Cole DenBleyker
+     */
     boolean removeFromCart (int id, String usernam) throws IOException;
 }
