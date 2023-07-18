885ebe1571c7da0f03d4666fe09baa4fe8331517
Ryan Robison <ryanrobison10@gmail.com>
2023-07-17 22:45:19 -0400

    changed cart service so it functions with whoever is logged in

--- estore-ui/angular-bookstore-ui/src/app/cart.service.ts
+++ estore-ui/angular-bookstore-ui/src/app/cart.service.ts
@@ -4,6 +4,7 @@ import { Product } from './product'
 
 import {Observable, of} from 'rxjs';
 import { catchError, map, tap } from 'rxjs/operators';
+import { AppComponent } from './app.component';
 
 @Injectable({
   providedIn: 'root'
@@ -13,7 +14,7 @@ export class CartService {
   *Url and user are defined here
   */
   private productURL = 'http://localhost:8080/estore';
-  private user = 'admin';
+  private user = AppComponent.getUsername();
 
   httpOptions = {
     headers: new HttpHeaders({ 'Content-Type': 'application/json' })
@@ -26,6 +27,7 @@ export class CartService {
   *Add to cart takes in product and alters url so post mapping adds specified product to cart
   */
   addToCart(product: Product): Observable<Product> {
+    this.user = AppComponent.getUsername();
     const url = `${this.productURL}/carts/${this.user}/product`
     return this.http.post<Product>(url,product,this.httpOptions).pipe(
       tap(),
@@ -38,6 +40,7 @@ export class CartService {
   *specified product
   */
   removeFromCart(product: Product): Observable<Product> {
+    this.user = AppComponent.getUsername();
     const url = `${this.productURL}/carts/${this.user}/product/${product.id}`
     return this.http.delete<Product>(url).pipe(
       tap(),
