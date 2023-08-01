fe57b7bf14d29193d0aeb2a962ef71fe0dcea0c7
Rylan <rma6182@rit.edu>
2023-08-01 19:45:20 -0400

    Added tests for DiscountCodeFileDAO

--- estore-api/src/test/java/com/estore/api/estoreapi/persistence/DiscountCodeFileDAOTest.java
+++ estore-api/src/test/java/com/estore/api/estoreapi/persistence/DiscountCodeFileDAOTest.java
@@ -3,6 +3,7 @@ package com.estore.api.estoreapi.persistence;
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -26,8 +27,8 @@ public class DiscountCodeFileDAOTest {
                 mockObjectMapper = mock(ObjectMapper.class);
                 testCodes = new DiscountCode[3];
                 testCodes[0] = new DiscountCode("ab1", 10);
-                testCodes[1] = new DiscountCode("sh2", 20);
-                testCodes[2] = new DiscountCode("ab2", 15);
+                testCodes[1] = new DiscountCode("ab2", 15);
+                testCodes[2] = new DiscountCode("sh2", 20);
 
                 // When the object mapper is supposed to read from the file
                 // the mock object mapper will return the product array above
@@ -39,7 +40,70 @@ public class DiscountCodeFileDAOTest {
         
         }
 
-    
+    /**
+     * Tests retrieving all discount codes.
+     */
+    @Test
+    public void testGetDiscountCodes() {
+        // invoke
+        DiscountCode[] result = assertDoesNotThrow(() -> discountFileDAO.getDiscountCodes(),
+                        "Unexpected exception thrown");
+
+        // analyze
+        assertArrayEquals(testCodes, result);
+        assertEquals(testCodes.length, result.length);
+    }
+
+    /**
+     * Tests retrieving a specific discount code.
+     */
+    @Test
+    public void testFindDiscountCode() {
+        // setup
+        String searchString = "ab1";
+
+        // invoke
+        DiscountCode result = assertDoesNotThrow(() -> discountFileDAO.findDiscountCode(searchString),
+                        "Unexpected exception thrown");
+
+        // analyze
+        assertEquals(testCodes[0], result);
+    }
+
+    /**
+     * Tests retrieving a specific discount code that doesn't exist.
+     */
+    @Test
+    public void testFindDiscountCode_invalid() {
+        // setup
+        String searchString = "blah";
+
+        // invoke
+        DiscountCode result = assertDoesNotThrow(() -> discountFileDAO.findDiscountCode(searchString),
+                        "Unexpected exception thrown");
+
+        // analyze
+        assertNotEquals(testCodes[0], result);
+        assertNotEquals(testCodes[1], result);
+        assertNotEquals(testCodes[2], result);
+    }
+
+    /**
+     * Tests creating a discount code.
+     */
+    @Test
+    public void testCreateDiscountCode() {
+        // setup
+        testCodes[2] = new DiscountCode("NewCode", 50);
+
+        // invoke
+        DiscountCode result = assertDoesNotThrow(() -> discountFileDAO.createDiscountCode(testCodes[2]),
+                        "Unexpected exception thrown");
+
+        // analyze
+        assertEquals(testCodes[2].toString(), result.toString());
+    }
+
     /**
      * Tests removing a product from the cart
      */
