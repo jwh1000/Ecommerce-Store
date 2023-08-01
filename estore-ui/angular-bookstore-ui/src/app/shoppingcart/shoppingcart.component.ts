c128149f688d4ed9c7fc56c4ec0e8d5f6b22dcc1
xhh9850 <xhh9850@g.rit.edu>
2023-08-01 15:03:25 -0400

    generated_comp

---
+++ estore-ui/angular-bookstore-ui/src/app/shoppingcart/shoppingcart.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-shoppingcart',
+  standalone: true,
+  imports: [CommonModule],
+  template: `
+    <p>
+      shoppingcart works!
+    </p>
+  `,
+  styleUrls: ['./shoppingcart.component.css']
+})
+export class ShoppingcartComponent {
+
+}
